e17da1a233dadcccf95dddf6580dfaf657b23c74
datadeer <konto@datadeer.de>
2020-08-22 17:36:49 +0200

    * add css reset * add web font

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,22 @@ import Navbar from "@/components/Navbar.vue";
 
 @Component({
   components: {
-    Navbar,
+    Navbar
   }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "./css/reset.scss";
+@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;400;600;700&display=swap");
+</style>
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Source Sans Pro", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
