574ede9ebe2d9d3467c1a3b7574782cb20a79c23
datadeer <konto@datadeer.de>
2020-08-22 17:08:42 +0200

    * add i18n dependcies * remove redundant code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Navbar from "@/components/Navbar.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Navbar,
   }
 })
 export default class App extends Vue {}
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <p>{{ $t("account") }}</p>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped></style>
+
+<i18n>
+{
+"en": {
+"account": "account"
+},
+"de": {
+"account": "konto"
+}
+}
+</i18n>
